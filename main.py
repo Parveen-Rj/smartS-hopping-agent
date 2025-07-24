f19400e6c0652955785921c978a9f2052b8f1054
Parveen-Rj <165608618+Parveen-Rj@users.noreply.github.com>
2025-07-24 15:13:54 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,3 @@
-# from litellm import completion
-# import os
-# from dotenv import load_dotenv
-
-# load_dotenv()
-
-# def main():
-#     api_key = os.getenv("GEMINI_API_KEY")
-#      # 👇 User se sawal lena
-#     user_question = input("Apna sawal poochhein: ")
-
-
-
-#     response = completion(
-#         model="gemini/gemini-2.0-flash",
-#         messages=[
-#             {
-#                 "role": "user",
-#                 "content":"user_question"
-#             }
-#         ],
-#     )
-#     print("Jawab:",response['choices'][0]['message']['content'])
-# if __name__ == "__main__":
-#     main()
-
-# 
-
 
 import streamlit as st
 from litellm import completion
@@ -40,7 +12,7 @@ st.set_page_config(page_title="🛍️ Smart Shopping Agent", page_icon="🛒")
 st.title("🛒 Smart Shopping Agent")
 st.markdown("Ask about **products, prices, or shopping suggestions**:")
 
-user_input = st.text_input("🔍 What are you looking for?", placeholder="e.g. School shoes under 5000")
+user_input = st.text_input("🔍 What are you looking for?", placeholder="What are you shopping for? ")
 
 if st.button("🧠 Find Products"):
     if user_input.strip() != "":
